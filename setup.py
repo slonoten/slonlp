6d6c35a77b40f4c7652484580ac6124527cf8d2e
Your Name <slonoten@gmail.com>
2020-03-02 00:02:57 +0300

    setup.py added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='slonlp',
+    version='0.1dev',
+    packages=[
+        'slonlp',
+    ],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description='Experiments with different NLP tasks.',
+)
